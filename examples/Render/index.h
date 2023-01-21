01499e20766230e024b5c5334836bdb7a9cd5efd
lathoub <4082369+lathoub@users.noreply.github.com>
2023-01-21 08:35:14 +0100

    initial upload (from Express)

---
+++ examples/Render/index.h
@@ -0,0 +1,13 @@
+class index {
+public:
+  static const char* file() {
+    return "<html>\n"
+           "<head>\n"
+           "<title>{{een}}</title>\n"
+           "</head>\n"
+           "<body>\n"
+           "<h2>{{twee}}</h2>\n"
+           "</body>\n"
+           "</html>";
+  }
+};
